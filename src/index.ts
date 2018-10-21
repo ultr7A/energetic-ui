5d571849cb20836f76c6305ea4049f96832b2a1d
Jeremy Evans <openspacehexagon@gmail.com>
2018-10-21 14:23:03 -0400

    missed one

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export { Button } from "./components/button/button";
 export { FileButton } from "./components/button/file-button";
 export { Card } from "./components/card/card";
 export { ContextMenu } from "./components/card/context-menu";
+export { VectorInput } from "./components/input/vector-input";
 export { Shell } from "./components/container/shell";
 export { LocationBar } from "./components/container/location-bar";
 export { MenuItem } from "./components/nav/menu-item";
