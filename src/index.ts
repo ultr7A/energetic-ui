c1bfc169bf0b1f2decdb0cc0c95d798455aeaef7
Jeremy Evans <openspacehexagon@gmail.com>
2018-11-04 15:58:06 -0500

    missed an import

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export { FileButton } from "./components/button/file-button";
 export { Card } from "./components/card/card";
 export { ContextMenu } from "./components/card/context-menu";
 export { VectorInput } from "./components/input/vector-input";
+export { ToggleInput } from "./components/input/toggle-input";
 export { Shell } from "./components/container/shell";
 export { LocationBar } from "./components/container/location-bar";
 export { MenuItem } from "./components/nav/menu-item";
