8b7fa17b1bd19f042c68f2b0a66f03fbd189ffd4
Jeremy Evans <openspacehexagon@gmail.com>
2020-02-08 18:09:49 -0500

    make panel better

--- src/components/panel/panel.tsx
+++ src/components/panel/panel.tsx
@@ -1,4 +1,5 @@
 import * as React from "react"; import { Component } from "react";
+import { Card } from "../card/card";
 
 const panelStyle = {
     display: "inline-block",
@@ -6,12 +7,13 @@ const panelStyle = {
     overflow: "visible"
 };
 const controlStyle = {
-    display: "block"
+    display: "inline-block",
+    float: "right",
 };
 
 export interface PanelProps {
+    title?: string;
     customStyle?: CSSStyleDeclaration;
-    customExpandedWidth?: string;
     controlStyle?: CSSStyleDeclaration;
 }
 
@@ -38,7 +40,6 @@ export class Panel extends Component<PanelProps, PanelState> {
         const style = {...panelStyle, width: "1em", height: "1em", overflow: "hidden", ...(this.props.customStyle ? this.props.customStyle : {})};
 
         if (this.state.open) {
-            style.width = this.props.customExpandedWidth ? this.props.customExpandedWidth : "5em";
             style.height = "auto";
             style.overflow = "visible";
         } 
@@ -49,6 +50,11 @@ export class Panel extends Component<PanelProps, PanelState> {
     render() {
         return (
             <aside style={this.computeStyle() as any}>
+                <Card 
+                    showTitle={true} 
+                    title={this.props.title || "Panel"} 
+                    clickHandler={()=> this.onToggle()} 
+                />
                 <div style={{ ...controlStyle, ...(this.props.controlStyle ? this.props.controlStyle : {})} as any}
                     onPointerDown={(e)=> this.onToggle()}
                 >
